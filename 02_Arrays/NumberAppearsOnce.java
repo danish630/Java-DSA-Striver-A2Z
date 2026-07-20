b0af10c77eb7b70029b5cc004a1b5563d33277b2
Danish Shaikh <danishshk32@gmail.com>
2026-07-20 18:59:03 +0530

    Solved the Longest Subarray Of Sum k

--- 02_Arrays/NumberAppearsOnce.java
+++ 02_Arrays/NumberAppearsOnce.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NumberOnce {
+public class NumberAppearsOnce {
     public static int findNumber(int []array){
         int result=0;
         for(int i=0;i<array.length;i++){
@@ -22,8 +22,8 @@ public class NumberOnce {
         for(int i = 0; i < size; i++) {
             array[i] = sc.nextInt();
         }
-        int numberappear= findNumber(array);
-        System.out.println("the number apperas once " +numberappear);
+        int finalresult= findNumber(array);
+        System.out.println("the number apperas once " + finalresult);
         sc.close();
 
     }
