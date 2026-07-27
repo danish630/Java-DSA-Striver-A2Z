9cd5e4f4c13dbbe3ecb49adcffdc084af70fd485
Danish Shaikh <danishshk32@gmail.com>
2026-07-27 16:51:09 +0530

    solved permutation problem

---
+++ 02_Arrays/Permutation.java
@@ -0,0 +1,11 @@
+public class Permutation {
+    public stat
+    
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3};
+        int n = array.length;
+        // Call the function to generate permutations
+        generatePermutations(array, 0, n);
+    }
+}
+
