6c7acb4cb810d7ebc412635c2db8fdec11b16403
Danish Shaikh <danishshk32@gmail.com>
2026-08-19 14:36:52 +0530

    Solved the Largest odd number substring

---
+++ Strings/LargestOddNumber.java
@@ -0,0 +1,28 @@
+package Strings;
+public class LargestOddNumber {
+    public static String largestOddNumber(String s) {
+        int right = s.length() - 1;
+        for (int i = right; i >= 0; i--) {
+            if ((s.charAt(i) - '0') % 2 != 0) {
+                return s.substring(0, i + 1);
+            }
+            
+
+
+
+
+    }
+    return "";
+}
+
+    public static void main(String[] args) {
+        String s = "35698242";
+        String result = largestOddNumber(s);
+        System.out.println(result);
+
+
+}
+}
+
+
+        
